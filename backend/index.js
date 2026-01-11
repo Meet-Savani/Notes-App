42ad302f0a22282b9aca6af2cad84950acb5fdbb
Meet Savani <meetsavani75@gmail.com>
2026-01-11 17:51:42 +0530

    Update CORS origin to allow frontend URL

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.BASE_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));

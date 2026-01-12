be22c153cd55b12485dab0db467773f2eea57e27
Meet Savani <meetsavani75@gmail.com>
2026-01-12 14:28:26 +0530

    FIX: remove nested BrowserRouter from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/" exact element={ <Navigate to="/dashboard" replace /> } />
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<SignUp />} />
-    </Routes>
-  </Router>
-);
-
 const App = () => {
   return (
-    <div>
-      {routes}
-    </div>
+    <Routes>
+      <Route path="/" element={ <Navigate to="/dashboard" replace /> } />
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   )
 }
 
